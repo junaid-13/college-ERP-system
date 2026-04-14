0704a44cc29182e4675b9984ec1d390be7c6627f
Ubuntu <ubuntu@ip-172-31-54-61.ec2.internal>
2026-04-14 10:47:06 +0000

    Written the code for prompt builder.

---
+++ services/prompt_builder.py
@@ -0,0 +1,15 @@
+def buil_prompt(config: dict, user_prompt: str):
+    return f"""
+You are a senior full-stack developer.
+
+project: {config['project_name']}
+Stack: {config['stack']}
+
+Instructions:
+{config['system_prompt']}
+
+user Request:
+{user_prompt}
+
+Provide clean, production-ready code.
+"""
