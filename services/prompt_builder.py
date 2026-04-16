3661012c6273f4109affe9ec5be2cd7c3bf0251e
Ubuntu <ubuntu@ip-172-31-54-61.ec2.internal>
2026-04-16 05:38:16 +0000

    Modified the code regarding addition of context related files.

--- services/prompt_builder.py
+++ services/prompt_builder.py
@@ -1,4 +1,4 @@
-def build_prompt(config: dict, memory: dict, user_prompt: str):
+def build_prompt(config: dict, memory: dict, code_context: str,user_prompt: str):
     return f"""
 You are a senior full-stack developer.
 
@@ -17,9 +17,13 @@ Current Feature:
 Last Task:
 {memory.get('last_task')}
 
+{code_context}
+
 user Request:
 {user_prompt}
 
-Continue the project intelligently based on current progress.
+If context is insufficient, ask for specific files.
 Provide clean, production-ready code.
 """
+# Continue the project intelligently based on current progress.
+# The above line has been removed. If want to reuse then you can add it above the provide clean, production-ready code.
